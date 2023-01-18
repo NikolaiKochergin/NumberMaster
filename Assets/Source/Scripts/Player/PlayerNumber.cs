9647798aaf1d3a3e848e2ade2bc1f86292267d93
Nikolai Kochergin <Kocherginizer@gmail.com>
2023-01-18 11:54:35 +0300

    Add player number calculating mechanic

--- Assets/Source/Scripts/Player/PlayerNumber.cs
+++ Assets/Source/Scripts/Player/PlayerNumber.cs
@@ -1,9 +1,45 @@
+using System;
+using Source.Scripts.Data;
+using Source.Scripts.Services.PersistentProgress;
 using UnityEngine;
 
 namespace Source.Scripts.Player
 {
-    public class PlayerNumber : MonoBehaviour
+    public class PlayerNumber : MonoBehaviour , ISavedProgress
     {
-    
+        private State _state;
+
+        public event Action NumberChanged;
+
+        public int Current
+        {
+            get => _state.CurrentNumber;
+            set
+            {
+                if (value != _state.CurrentNumber)
+                {
+                    _state.CurrentNumber = value;
+                    
+                    NumberChanged?.Invoke();
+                }
+            }
+        }
+
+        public void LoadProgress(PlayerProgress progress)
+        {
+            _state = progress.PlayerState;
+            NumberChanged?.Invoke();
+        }
+
+        public void UpdateProgress(PlayerProgress progress) => 
+            progress.PlayerState.CurrentNumber = Current;
+
+        public void TakeNumber(int value)
+        {
+            if(Current <= 0)
+                return;
+
+            Current += value;
+        }
     }
 }
