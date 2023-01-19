87b01a204c97ccb0024cba24d05813714121cc7b
Евгений <listopadov_94@mail.ru>
2023-01-19 12:30:40 +0300

    финшь

---
+++ Assets/Source/Scripts/PlayerLogic/PlayerMoveFinish.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveFinish : MonoBehaviour
+{
+    [SerializeField] private float _positionX;
+}
