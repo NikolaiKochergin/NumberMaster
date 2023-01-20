c242d0187ec8ff0c9b227c7c3b13a38127d614c9
Евгений <listopadov_94@mail.ru>
2023-01-20 15:04:48 +0300

    лидерборд

---
+++ Assets/Source/Scripts/Leaderboard/LeaderboardName.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Source.Scripts.Leaderboard
+{
+    public class LeaderboardName : MonoBehaviour
+    {
+        public const string Name = "NumberMaster";
+    }
+}
---
+++ Assets/Source/Scripts/Leaderboard/LeaderboardScoreSaver.cs
@@ -0,0 +1,49 @@
+using Source.Scripts.Services;
+using Source.Scripts.Services.PersistentProgress;
+using UnityEngine;
+
+namespace Source.Scripts.Leaderboard
+{
+    public class LeaderboardScoreSaver : MonoBehaviour
+    {
+        [SerializeField] private float _timeBetweenSaves;
+
+        private int _totalScore;
+        private int _lastSavedScore;
+        private IPersistentProgressService _progressService;
+
+        private void Awake()
+        {
+            _progressService = AllServices.Container.Single<IPersistentProgressService>();
+        }
+
+        public void Load()
+        {
+            _totalScore = _progressService.Progress.World.DisplayedLevel;
+
+            _lastSavedScore = _totalScore;
+        }
+
+        public void StashScore(int score)
+        {
+            _totalScore = _progressService.Progress.World.DisplayedLevel;
+            Save();
+        }
+
+        private void OnCompleted()
+        {
+            Save();
+        }
+
+        private void Save()
+        {
+#if !UNITY_EDITOR && UNITY_WEBGL
+        if (PlayerAccount.IsAuthorized && _lastSavedScore != _totalScore)
+        {
+            Leaderboard.SetScore(LeaderboardName.Name, _totalScore);
+            _lastSavedScore = _totalScore;
+        }
+#endif
+        }
+    }
+}
