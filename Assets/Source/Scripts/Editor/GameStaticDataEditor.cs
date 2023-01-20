1dfe96eedc10aaa48e963dc031ff7ad7f18150c7
Nikolai Kochergin <Kocherginizer@gmail.com>
2023-01-20 13:39:34 +0300

    Add editor Tool for scene

---
+++ Assets/Source/Scripts/Editor/GameStaticDataEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Source.Scripts.StaticData;
+using UnityEditor;
+using UnityEngine;
+
+namespace Source.Scripts.Editor
+{
+    [CustomEditor(typeof(GameStaticData))]
+    public class GameStaticDataEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GameStaticData gameData = (GameStaticData) target;
+
+            if (GUILayout.Button("Collect"))
+            {
+                gameData.LevelSceneNames.Clear();
+                
+                EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+
+                foreach (EditorBuildSettingsScene scene in scenes)
+                    gameData.LevelSceneNames.Add(Path.GetFileNameWithoutExtension(scene.path));
+            }
+            
+            EditorUtility.SetDirty(target);
+        }
+    }
+}
